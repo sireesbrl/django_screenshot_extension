b0faaf2a9500db7483d0d1c40d95071f9c85d7f9
Sirees <sireesbrl@gmail.com>
2025-02-09 16:54:46 +0545

    update celery tasks

--- api/tasks.py
+++ api/tasks.py
@@ -2,10 +2,15 @@ from .models import Screenshot
 from celery import shared_task
 from django.utils import timezone
 from datetime import timedelta
+import os
 
 @shared_task
 def delete_old_screenshots():
     threshold_date = timezone.now() - timedelta(days=30)
     old_screenshots = Screenshot.objects.filter(uploaded_at__lt=threshold_date)
+    for screenshot in old_screenshots:
+        image_path = screenshot.image.path
+        if os.path.exists(image_path):
+            os.remove(image_path)
     old_screenshots.delete()
     return f"Deleted {len(old_screenshots)} screenshots older than 30 days"
