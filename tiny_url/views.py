5db45a84b283e2a5aa81b6d8027f42961f336aca
Sirees <sireesbrl@gmail.com>
2025-02-20 13:21:45 +0545

    refactor code

--- tiny_url/views.py
+++ tiny_url/views.py
@@ -1,9 +1,10 @@
-from .models import TinyUrl
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponseNotAllowed
+from tiny_url.models import TinyUrl
 
-def get_image(request, tiny_url):
-    if request.method == "GET":
-        tinyurl = get_object_or_404(TinyUrl, short_url=tiny_url)
-        original_url = tinyurl.original_url
-        return HttpResponseRedirect(original_url)
+def get_image(request, short_url):
+    if request.method != 'GET':
+        return HttpResponseNotAllowed("Method Not Allowed")
+    
+    tiny_url = get_object_or_404(TinyUrl, short_url=short_url)
+    return HttpResponseRedirect(tiny_url.original_url)
