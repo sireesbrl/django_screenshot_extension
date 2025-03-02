34c58b31b88464f84edadc0d15e40f6f234dddb5
Sirees <sireesbrl@gmail.com>
2025-03-02 14:18:33 +0545

    refactor code for image streaming

--- tiny_url/views.py
+++ tiny_url/views.py
@@ -1,20 +1,31 @@
+import requests
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponseNotAllowed
+from django.http import HttpResponse, HttpResponseNotAllowed
 from tiny_url.models import TinyUrl
 
 def get_image(request, tiny_url):
     """
-    Redirects a GET request with a short_url to the original_url associated
-    with that short_url.
+    View to handle GET requests to the /{tiny_url} endpoint.
 
-    If the request is not a GET, returns a 405 Method Not Allowed response.
-
-    :param request: The request object
-    :param short_url: The short_url to redirect
-    :return: A redirect response to the original_url
+    Fetches the image at the original URL associated with the given short URL,
+    and returns it to the client with a Content-Type header indicating the
+    MIME type of the image. If the request is not a GET request, returns a 405
+    status code. If the request is invalid or if there is an error fetching the
+    image, returns a 400 or 500 status code respectively.
     """
     if request.method != 'GET':
-        return HttpResponseNotAllowed("Method Not Allowed")
-    
-    tiny_url = get_object_or_404(TinyUrl, short_url=tiny_url)
-    return HttpResponseRedirect(tiny_url.original_url)
+        return HttpResponseNotAllowed(["GET"])
+
+    tiny_url_obj = get_object_or_404(TinyUrl, short_url=tiny_url)
+    image_url = tiny_url_obj.original_url
+
+    try:
+        response = requests.get(image_url, stream=True)
+        response.raise_for_status()
+
+        content_type = response.headers.get("Content-Type", "image/jpeg")
+
+        return HttpResponse(response.content, content_type=content_type)
+
+    except requests.RequestException as e:
+        return HttpResponse(f"Error fetching image: {str(e)}", status=500)
